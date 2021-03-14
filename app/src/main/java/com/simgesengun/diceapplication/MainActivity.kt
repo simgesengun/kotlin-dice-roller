1f2d62b4acc33cb5113c4f32c52962313de57dc6
simgesengun <simgesngn@gmail.com>
2021-03-14 16:02:23 +0300

    Commit on 'main'

---
+++ app/src/main/java/com/simgesengun/diceapplication/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.simgesengun.diceapplication
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.util.*
+import java.util.Calendar.HOUR_OF_DAY
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val dice1Image: ImageView = findViewById(R.id.dice1Image)
+        val dice2Image: ImageView = findViewById(R.id.dice2Image)
+        val rollButton: Button = findViewById(R.id.rollButton)
+        val clearButton: Button = findViewById(R.id.clearButton)
+
+        val dice1 = Dice(6)
+        val dice2 = Dice(6)
+        rollButton.setOnClickListener{
+            rollDice(dice1, dice1Image)
+            rollDice(dice2, dice2Image)
+
+            Toast.makeText(this@MainActivity, "Zar Atıldı!", Toast.LENGTH_SHORT).show()
+        }
+
+        clearButton.setOnClickListener{
+            dice1Image.setImageResource(R.drawable.empty_dice)
+            dice2Image.setImageResource(R.drawable.empty_dice)
+        }
+    }
+
+    //Zarı ve ImageView'ını alarak ImageView'ı değiştirir
+    fun rollDice(dice: Dice, diceImage : ImageView){
+        when(dice.roll()) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            else -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+    }
+
+
+}
